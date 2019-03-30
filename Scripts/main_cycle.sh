93e037f4a9c6ab8f9cd5fabff168dba4a624eacc
owerbat <niktimak@gmail.com>
2019-03-30 19:04:06 +0300

    Bash script which calculates average times added

---
+++ Scripts/main_cycle.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ -n "$1" ]
+then
+number=$1
+else
+number=10
+fi
+
+#tput sc
+
+for (( i=1; i <= $number; i++ ))
+do
+../Source/main
+
+percent=$(printf %.0f\\n "$((10**2 * i/number))")
+echo -ne "\rProgress: $percent%"
+done
+
+echo
+
+python calculate_average_times.py
+
+cat ../Results/txt/average_times.txt
